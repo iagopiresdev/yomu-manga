6d9861f5f8c131d9bf24d9a617c99645bd6f4c26
Iago Pires <iagopires.dev@gmail.com>
2023-06-05 11:02:55 -0400

    login endpoint now returns specific error messages

--- server/src/services/authUser/AuthUser.ts
+++ server/src/services/authUser/AuthUser.ts
@@ -1,43 +1,47 @@
-
 import { client } from "../../prisma/client";
 import { compare } from "bcryptjs";
 import { GenerateRefreshToken } from "../../providers/GenerateRefreshToken";
 import { GenerateTokenProvider } from "../../providers/GenerateTokenProvider";
 
-
 interface IAuthUserRequest {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 class AuthUser {
-    async execute({email, password}: IAuthUserRequest) {
-        const userAlreadyExists = await client.user.findFirst({
-            where: {
-                email
-            }
-        });
+  async execute({ email, password }: IAuthUserRequest) {
+    const userAlreadyExists = await client.user.findFirst({
+      where: {
+        email,
+      },
+    });
 
-        if(!userAlreadyExists) {
-            throw new Error("Email/Password incorrect");
-        }
-
-        const passwordMatch = await compare(password, userAlreadyExists.password);
-
-        if(!passwordMatch) {
-            throw new Error("Email/Password incorrect");
-        }
-        
-        //gerar token
-        const generateTokenProvider = new GenerateTokenProvider();
-        const token = await generateTokenProvider.execute(userAlreadyExists.id);
-        
-        //gerar refresh token
-        const generateRefreshToken = new GenerateRefreshToken();
-        const refreshToken = await generateRefreshToken.execute(userAlreadyExists.id);
-
-    return { token, refreshToken }
+    if (!userAlreadyExists) {
+      throw {
+        errorCode: "INVALID_EMAIL_PASSWORD",
+        errorMessage: "Email/Password incorrect",
+      };
     }
+
+    const passwordMatch = await compare(password, userAlreadyExists.password);
+
+    if (!passwordMatch) {
+      throw {
+        errorCode: "INVALID_EMAIL_PASSWORD",
+        errorMessage: "Email/Password incorrect",
+      };
+    }
+
+    // Generate token
+    const generateTokenProvider = new GenerateTokenProvider();
+    const token = await generateTokenProvider.execute(userAlreadyExists.id);
+
+    // Generate refresh token
+    const generateRefreshToken = new GenerateRefreshToken();
+    const refreshToken = await generateRefreshToken.execute(userAlreadyExists.id);
+
+    return { token, refreshToken };
+  }
 }
 
-export { AuthUser }
+export { AuthUser };
